9831fcdfaaa3e31b8264802431c9a095997ae6a9
Piero Narciso <pieronarciso988@gmail.com>
2022-02-21 22:08:16 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+target=$HOME/.local/bin
+filename=ssh-sync
+
+mkdir -p $target
+curl -o $target/$filename https://raw.githubusercontent.com/PieroNarciso/ssh-sync/main/ssh-sync
+
+chmod +x $target/$filename
